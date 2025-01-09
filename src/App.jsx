1e876640cc571450b74cf2295df489b1d06d9cff
iculmination <ovsiichuk.bohdan@gmail.com>
2025-01-09 18:30:59 +0200

    prev commit redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import About from "./components/About";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
